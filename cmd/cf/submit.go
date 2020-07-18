ba942e1a1b420177c148a4337a6564589fb16b0c
Adithya Joshua Dsilva <adithyadsilva777@gmail.com>
2020-07-19 01:56:35 +0530

    add submit - cf

--- cmd/cf/submit.go
+++ cmd/cf/submit.go
@@ -2,6 +2,7 @@ package cf
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"time"
 
@@ -11,6 +12,7 @@ import (
 	"github.com/gosuri/uilive"
 	"github.com/gosuri/uitable"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // submitCmd refers to 'submit' command
@@ -34,72 +36,71 @@ func submit(spfr, workDir string) {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	/*
-		if len(arg.Contest) == 0 {
-			fmt.Println("Contest id not specified")
-			os.Exit(1)
-		} else if len(arg.Problem) == 0 {
-			fmt.Println("Problem id not specified")
-			os.Exit(1)
-		}
 
-		// find all code files in current directory, if file not specified
-		file, _ := lFlags.GetString("file")
-		file, err = util.FindCodeFiles(file)
+	if len(arg.Contest) == 0 {
+		fmt.Println("Contest id not specified")
+		os.Exit(1)
+	} else if len(arg.Problem) == 0 {
+		fmt.Println("Problem id not specified")
+		os.Exit(1)
+	}
+
+	// find all code files in current directory (check if given 'file' is valid)
+	file, _ := lFlags.GetString("file")
+	file, err = util.FindCodeFiles(file)
+	if err != nil {
+		fmt.Println("Could not select code file")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// find template configuration to use
+	tmpltAlias, err := util.FindTemplateToUse(file)
+	if err != nil {
+		fmt.Println("Could not select template configuration")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// read selected source file (and submit)
+	sourceData, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println("Could not read code file")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Checking login status")
+	if usr, passwd := cfViper.GetString("username"), cfViper.GetString("password"); len(usr) == 0 || len(passwd) == 0 {
+		fmt.Println("Could not find any saved login credentials")
+		os.Exit(1)
+	} else {
+		passwd, err := util.Decrypt(usr, passwd)
 		if err != nil {
-			fmt.Println("Could not select code file")
+			fmt.Println("Could not decrypt password")
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		// find template configuration to use
-		tmpltAlias, err := util.FindTemplateToUse(file)
+		handle, err := codeforces.Login(usr, passwd)
 		if err != nil {
-			fmt.Println("Could not select template configuration")
+			fmt.Println("Could not check login status")
 			fmt.Println(err)
 			os.Exit(1)
 		}
+		// current user is loaded correctly
+		fmt.Println("Current user:", handle)
+	}
 
-		// read selected source file (and submit)
-		sourceData, err := ioutil.ReadFile(file)
-		if err != nil {
-			fmt.Println("Could not read code file")
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		fmt.Println("Checking login status")
-		if usr, passwd := cfViper.GetString("username"), cfViper.GetString("password"); len(usr) == 0 || len(passwd) == 0 {
-			fmt.Println("Could not find any saved login credentials")
-			os.Exit(1)
-		} else {
-			passwd, err := util.Decrypt(usr, passwd)
-			if err != nil {
-				fmt.Println("Could not decrypt password")
-				fmt.Println(err)
-				os.Exit(1)
-			}
-
-			handle, err := codeforces.Login(usr, passwd)
-			if err != nil {
-				fmt.Println("Could not check login status")
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			// current user is loaded correctly here
-			fmt.Println("Current user:", handle)
-			viper.WriteConfig()
-		}
-
-		tmplt := viper.GetStringMap("templates")[tmpltAlias].(map[string]interface{})
-		langName := tmplt["languages"].(map[string]interface{})["codeforces"].(string)
-		err = arg.SubmitSolution(codeforces.LanguageID[langName], string(sourceData))
-		if err != nil {
-			fmt.Println("Could not submit code")
-			fmt.Printf("%+q\n", err)
-			os.Exit(1)
-		}
-	*/
+	tmplt := viper.GetStringMap("templates")[tmpltAlias].(map[string]interface{})
+	langName := tmplt["languages"].(map[string]interface{})["codeforces"].(string)
+	err = arg.SubmitSolution(codeforces.LanguageID[langName], string(sourceData))
+	if err != nil {
+		fmt.Println("Could not submit code")
+		fmt.Printf("%+q\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Submitted")
 
 	// watch submission row, and print data
 	writer := uilive.New()
