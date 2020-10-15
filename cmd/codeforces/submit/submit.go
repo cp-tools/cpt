4a13755ed0065ca7ef17e08c82b6b29a03d9ffad
Adithya Joshua Dsilva <adithyadsilva777@gmail.com>
2020-10-15 17:32:50 +0530

    Multiple minor changes. * Fix map data merging in conf. * Change keys in generated meta.yaml. * Add submit subroutine (WIP).

---
+++ cmd/codeforces/submit/submit.go
@@ -0,0 +1,11 @@
+package submit
+
+import (
+	"github.com/cp-tools/cpt-lib/codeforces"
+	"github.com/cp-tools/cpt/packages/conf"
+)
+
+// Submit submits
+func Submit(arg codeforces.Args, filePath string, cnf *conf.Conf) {
+
+}
